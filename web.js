da55a68e8634c2aa9876879d01b288bfee0a177b
Ubuntu <ubuntu@ip-172-31-16-176.us-west-2.compute.internal>
2013-07-11 06:45:21 +0000

    Added capability to read index.html

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(fs.readFile('index.html'));
- 
+var str = fs.readFile('index.html');
+var buf = new Buffer(str,'utf-8'); 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
