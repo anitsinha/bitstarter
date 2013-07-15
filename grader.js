8c466905b7d5faea94d26106c4e3d385b372826f
Anit Sinha <anit.sinha@gmail.com>
2013-07-15 06:48:02 +0000

    Finally grader.js works. Sort of kludgy as it writes a file ) but still it works

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var path = require('path');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -39,6 +41,7 @@ var assertFileExists = function(infile) {
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -55,6 +58,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +69,32 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <website_url>','Url to be Checked' )
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+
+    if(program.url) {
+        console.log("url was passed");
+        var file = '_temp_index.html';
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+               sys.puts('Error: ' + result.message);
+               this.retry(5000); // try again after 5 sec
+            } else {
+               fd = fs.openSync(path.join(process.cwd(),file),'w+');
+               fs.writeSync(fd,result);
+               fs.closeSync(fd);
+            };
+       });
+
+        var checkJson =checkHtmlFile(file, program.checks);
+    } else {
+        console.log("file was passed");
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    };
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    fs.writeFileSync('out.json',outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
